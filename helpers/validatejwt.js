cd31161cf32b8bedd3930bde4ca0f896e72bcf70
Anshuk2003 <anshukvkalbande114@gmail.com>
2024-07-08 18:14:57 +0530

    Commit jwt validation

--- helpers/validatejwt.js
+++ helpers/validatejwt.js
@@ -1,20 +1,4 @@
-// import jwt from "jsonwebtoken"
- import { NextResponse } from "next/server";
-
-// export const validateJWT = async (request) => {
-//     try {
-//         console.log(request.cookies);
-//         const currenttoken = request.cookies.get("token")?.value;
-//         console.log(currenttoken)
-//         if (currenttoken) {
-//             const decodedata = await jwt.verify(currenttoken, process.env.JWT_SECRETE);
-//             return decodedata.email;
-//         }
-//         return NextResponse.json({ message: 'Token not found' }, { status: 404 })
-//     } catch (error) {
-//         return new Error(error.message);
-//     }
-// }
+import { NextResponse } from "next/server";
 import jwt from "jsonwebtoken";
 import cookie from "cookie";
 
@@ -22,7 +6,7 @@ export const validateJWT = async (request) => {
     try {
         const cookies = request.headers.get('cookie');
         if (!cookies) {
-            return NextResponse.json({ message: 'cookies not found' }, { status: 404 })
+            return NextResponse.json({ message: 'Token not found' }, { status: 404 })
         }
 
         const parsedCookies = cookie.parse(cookies);
@@ -35,7 +19,7 @@ export const validateJWT = async (request) => {
         const decodedData = jwt.verify(currentToken, process.env.JWT_SECRETE);
         return decodedData.email;
     } catch (error) {
-       //console.error("JWT validation error: ", error);
-       return NextResponse.json({ message: 'JWT validation error: invalid token' }, { status: 500 })
+        //console.error("JWT validation error: ", error);
+        return NextResponse.json({ message: 'JWT validation error: invalid token' }, { status: 500 })
     }
 };
